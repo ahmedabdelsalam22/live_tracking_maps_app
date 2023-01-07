765f029255f599e33eb288d6dcd253bbf8613a29
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-01-07 13:25:08 +0200

    add emitPlaceSuggestions method to MapsCubit

--- lib/presentation/business_logic/google_maps/maps_cubit.dart
+++ lib/presentation/business_logic/google_maps/maps_cubit.dart
@@ -1,8 +1,20 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../data/model/place_suggestion_model.dart';
+import '../../../data/repository/maps_repository.dart';
 
 part 'maps_state.dart';
 
 class MapsCubit extends Cubit<MapsState> {
-  MapsCubit() : super(MapsInitial());
+  final MapsRepository mapsRepository;
+
+  MapsCubit(this.mapsRepository) : super(MapsInitial());
+
+  void emitPlaceSuggestions(String place, String sessionToken) {
+    mapsRepository.fetchSuggestionsPlaces(place, sessionToken).then(
+      (suggestions) {
+        emit(PlacesLoaded(suggestions));
+      },
+    );
+  }
 }
--- lib/presentation/business_logic/google_maps/maps_state.dart
+++ lib/presentation/business_logic/google_maps/maps_state.dart
@@ -1,6 +1,11 @@
 part of 'maps_cubit.dart';
 
-@immutable
 abstract class MapsState {}
 
 class MapsInitial extends MapsState {}
+
+class PlacesLoaded extends MapsState {
+  final List<PlaceSuggestionModel> places;
+
+  PlacesLoaded(this.places);
+}
